d723b89dd9f3cf318db785a63f2577bd71ecba1f
Andres Castañeda <af.castaneda223@gmail.com>
2021-12-13 10:56:02 -0500

    add navbar links, background, font

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 function navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark navcol border-0 mb-3 sticky">
       <div className="container">
-        <a className="navbar-brand fw-bolder fs-4" href="/#">Andres Castañeda</a>
+        <a className="navbar-brand fw-bolder fs-3" href="/#">Andres Castañeda</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav mx-auto">
+          <ul className="navbar-nav mx-auto fs-5">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="/#">Home</a>
             </li>
@@ -30,18 +30,15 @@ function navbar() {
               </ul>
             </li>
           </ul>
-          <div className="buttons">
-            <a href="/#" className="btn btn-outline-light me-2">
-              <i className="fas fa-sign-in-alt me-2" />
-              Login
+          <div className="buttons ">
+            <a href="https://api.whatsapp.com/send?phone=5491136572456" className="btn">
+              <i className="fab fa-whatsapp wapp fs-5" />
             </a>
-            <a href="/#" className="btn btn-outline-light me-2">
-              <i className="fas fa-user-plus me-2" />
-              Register
+            <a href="https://www.linkedin.com/in/andcast/" className="btn">
+              <i className="fab fa-linkedin fs-5 lkin" />
             </a>
-            <a href="/#" className="btn btn-outline-light me-2">
-              <i className="fas fa-shopping-cart me-2" />
-              Cart(0)
+            <a href="https://github.com/afcastaneda223" className="btn">
+              <i className="fab fa-github-square fs-5 text-white" />
             </a>
           </div>
         </div>
